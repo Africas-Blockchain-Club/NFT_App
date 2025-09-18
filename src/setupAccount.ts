403b3d13e04e3bd21176ab72f48ae2c6c337948a
sandileSiwundla <sandsmthembu2@gmail.com>
2025-09-18 17:53:09 +0200

    Your account was created successfully

--- src/setupAccount.ts
+++ src/setupAccount.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { createPublicClient, encodeFunctionData, http, parseAbi } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { scrollSepolia } from "viem/chains";
 import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
 import {
   createKernelAccount,
@@ -14,17 +14,19 @@ if (!process.env.ZERODEV_RPC) {
   throw new Error("ZERODEV_RPC is not set");
 }
 
+
+
 const ZERODEV_RPC = process.env.ZERODEV_RPC
 
 // The NFT contract we will be interacting with
-const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863";
+const contractAddress = "0xC9FEefAe44ddbA865e57209b6139a114662F4C81";
 const contractABI = parseAbi([
   "function mint(address _to) public",
   "function balanceOf(address owner) external view returns (uint256 balance)",
 ]);
 
 // Construct a public client
-const chain = sepolia;
+const chain = scrollSepolia;
 const publicClient = createPublicClient({
   transport: http(ZERODEV_RPC),
   chain,
