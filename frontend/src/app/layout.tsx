be248afcced82b4d167d9aced0f57a3411d232f2
sandileSiwundla <sandsmthembu2@gmail.com>
2025-09-22 23:27:59 +0200

    Added button pointer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { UserProvider } from "@/context/UserContext"; // Import the UserProvider
+import { UserProvider } from "@/context/UserContext"; 
+import Navbar from '@/components/Navbar';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <UserProvider> {/* Wrap children with UserProvider */}
+                  {/* <Navbar> */}
           {children}
+                  {/* </Navbar> */}
+
         </UserProvider>
       </body>
     </html>
